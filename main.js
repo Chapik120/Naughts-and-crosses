0be3da68ecff477eee1e74cace54ae2785d14713
Chapik120 <115930668+Chapik120@users.noreply.github.com>
2025-05-25 15:51:35 +0300

    Чутка оптимизировал
    if (e.target.className == "pole") {        
            return;
        }
        
        if (e.target.innerHTML !== "") {
            return;
        }
    
    переделал в:
    
    if (e.target.className == "pole" || e.target.innerHTML !== "") {
            return;
        }

---
+++ main.js
@@ -0,0 +1,52 @@
+const pole = document.querySelector(".pole");
+const span = document.getElementById("step");
+const winsX_html = document.getElementById("winsX");
+const winsO_html = document.getElementById("winsO");
+const switchMode = document.getElementById("switch");
+const winMass = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]];
+
+let xMoves = [];
+let oMoves = [];
+
+let winsX = 0;
+let winsO = 0;
+
+function checkWin() {
+    const divs = document.querySelectorAll(".pole div");
+    winMass.forEach((items, i) => {
+        if (divs[items[0] - 1].innerHTML == divs[items[1] - 1].innerHTML && divs[items[1] - 1].innerHTML == divs[items[2] - 1].innerHTML && divs[items[2] - 1].innerHTML !== "") {
+            if (divs[items[0] - 1].innerHTML == "✘") {
+                winsX += 1;
+                winsX_html.innerHTML = winsX;
+            } else {
+                winsO += 1;
+                winsO_html.innerHTML = winsO;
+            }
+            
+            alert(`WIN: ${divs[items[2] - 1].innerHTML}`);
+            divs.forEach(item => {
+                item.innerHTML = "";
+            });
+            simbol = "✘";
+            span.innerHTML = simbol;
+        }
+    });
+}
+
+let simbol = "✘";
+pole.addEventListener("click", (e) => {
+    if (e.target.className == "pole" || e.target.innerHTML !== "") {
+        return;
+    }
+    
+    e.target.innerHTML = simbol;
+    if (simbol == "✘") {
+        xMoves.push(e.target);
+        simbol = "Ｏ";
+    } else {
+        oMoves.push(e.target);
+        simbol = "✘";
+    }
+    span.innerHTML = simbol;
+    checkWin();
+});
